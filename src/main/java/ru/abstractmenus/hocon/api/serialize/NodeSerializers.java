8d62bee773087da27f67e64cbc8a60cc5f507e01
Nanit <nanitmix@gmail.com>
2022-01-03 15:22:05 +0200

    Added merge method for serialziers

--- src/main/java/ru/abstractmenus/hocon/api/serialize/NodeSerializers.java
+++ src/main/java/ru/abstractmenus/hocon/api/serialize/NodeSerializers.java
@@ -19,6 +19,13 @@ public class NodeSerializers {
     }
 
     /**
+     * Create new empty collection
+     */
+    public NodeSerializers() {
+        this(new HashMap<>());
+    }
+
+    /**
      * Get serializer of specified type
      * @param type Value type
      * @param <T> Value type
@@ -42,6 +49,18 @@ public class NodeSerializers {
     public <T> NodeSerializers register(Class<T> type, NodeSerializer<T> serializer) {
         serializers.put(type, serializer);
         return this;
+    }
+
+    /**
+     * Merge tho serializers collection to new collection
+     * @param serializers Another serializers collection
+     * @return New serializers collection with serializers both from current and another collection
+     */
+    public NodeSerializers merge(NodeSerializers serializers) {
+        NodeSerializers ser = new NodeSerializers();
+        ser.serializers.putAll(this.serializers);
+        ser.serializers.putAll(serializers.serializers);
+        return ser;
     }
 
     /**
